9354b83f7259eb59fe9ef49b67e9f3e1ceed4ebc
Sa Myeonggu(사명구) <quest4i@me.com>
2018-05-06 17:12:35 +0900

    setup main view

--- SelectCar/ViewController.swift
+++ SelectCar/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var imgView: UIImageView!
+
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 0
+    }
+
 
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        imgView.layer.cornerRadius = 50.0
+        imgView.layer.masksToBounds = true
     }
 
 
     override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
